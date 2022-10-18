9a95040e3ce5f142d3521e6bf5ac5c4aea55ea39
kingbowang <2508357595@qq.com>
2022-10-18 17:13:22 -0400

    a6 finish tuiter

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -9,7 +9,8 @@ const WhoToFollowListItem = (
         <li className="list-group-item">
             <div className="row">
                 <div className="col-2">
-                    <img className="rounded-circle" height={48} src={`/tuiter/pics/${who.avatarIcon}`}/>
+                    <img className="rounded-circle" height={48} src={`/tuiter/pics/${who.avatarIcon}`}
+                         alt='rounded_circle'/>
                 </div>
                 <div className="col-8">
                     <div className="fw-bold">{who.userName}</div>
