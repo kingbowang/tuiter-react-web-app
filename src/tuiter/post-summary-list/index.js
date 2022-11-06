8ab555ad874a75c6fe337ddee9510f0b8cf4ba80
kingbowang <2508357595@qq.com>
2022-11-06 13:25:00 -0500

    a7 modify explore

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,15 +1,25 @@
 import React from "react";
-import PostSummaryItem from "./post-summary-item";
+//import PostSummaryItem from "./post-summary-item";
+import TuitItem from "../tuits/tuitItem";
 import {useSelector} from "react-redux";
 
 const PostSummaryList = () => {
-    const postsArray = useSelector(state => state.tuits)
+    /*const postsArray = useSelector(state => state.tuits)
     return (
         <ul className="list-group">
             {
                 postsArray.map(post =>
                     <PostSummaryItem
                         key={post} post={post}/>)
+            }
+        </ul>
+    );*/
+    const postsArray = useSelector(state => state.homeTuits)
+    return (
+        <ul className="list-group">
+            {
+                postsArray.map(tuit =>
+                    <TuitItem key={tuit._id} tuit={tuit}/>)
             }
         </ul>
     );
